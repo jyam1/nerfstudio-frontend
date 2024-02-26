08973a0273f12bb45848e1a68c4de757784b7916
jyam1 <jasonyam51@gmail.com>
2024-02-26 11:03:50 -0800

    save file to current directory

--- nerfstudio_flask.py
+++ nerfstudio_flask.py
@@ -3,9 +3,22 @@ import os
 
 app = flask.Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home_page():
     return flask.render_template("frontend.html")
 
+@app.route('/', methods=['POST'])
+def send_video():
+    uploaded_video = flask.request.files['file']
+    
+    if uploaded_video.filename != "":
+        data_path = "./" + uploaded_video.filename + "_data"
+        
+        os.mkdir(data_path)
+        
+        uploaded_video.save(os.path.join(data_path, uploaded_video.filename))
+    
+    return flask.redirect(flask.url_for("home_page"))
+    
 if __name__ == "__main__":
     app.run(debug=True)
