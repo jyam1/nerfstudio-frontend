c9274b5d385d53baf06d148bd285031bd1804a18
jyam1 <jasonyam51@gmail.com>
2024-03-01 09:48:58 -0800

    File directory fix

--- nerfstudio_flask.py
+++ nerfstudio_flask.py
@@ -5,7 +5,7 @@ import os
 app = flask.Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home_page():
     return flask.render_template("frontend.html")
 
@@ -14,23 +14,25 @@ def send_video():
     uploaded_video = flask.request.files['file']
     
     if uploaded_video.filename != "":
-        data_path = "./" + uploaded_video.filename + "_data"
+        data_path = uploaded_video.filename + "_data"
         
         os.mkdir(data_path)
         
         uploaded_video.save(os.path.join(data_path, uploaded_video.filename))
         
-        output_path = "./" + uploaded_video.filename + "_output"
+        video_path = os.path.join(data_path, uploaded_video.filename)
+        
+        output_path = uploaded_video.filename + "_output"
     
     print("Using COLMAP to process video...")
-    ns_process_command = ["ns-process-data", "video", "--data", data_path, "--output-dir", output_path]
+    ns_process_command = ["ns-process-data", "video", "--data", video_path, "--output-dir", output_path]
     subprocess.run(ns_process_command)
     
     print("Training data...")
-    ns_train_command = ["ns-train", "splatfacto", "--data", data_path]
+    ns_train_command = ["ns-train", "splatfacto", "--data", output_path]
     subprocess.run(ns_train_command)
     
     return flask.redirect(flask.url_for("home_page"))
 
 if __name__ == "__main__": 
-    app.run(debug=True, host='0.0.0.0', port=7007)
+    app.run(debug=True, host='127.0.0.1', port=7007)
