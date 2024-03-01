b96409e5a39583cac79d52cc0fea94b38157be1e
jyam1 <jasonyam51@gmail.com>
2024-03-01 09:57:52 -0800

    Host fix

--- nerfstudio_flask.py
+++ nerfstudio_flask.py
@@ -35,4 +35,4 @@ def send_video():
     return flask.redirect(flask.url_for("home_page"))
 
 if __name__ == "__main__": 
-    app.run(debug=True, host='127.0.0.1', port=7007)
+    app.run(debug=True, host='0.0.0.0', port=7007)
