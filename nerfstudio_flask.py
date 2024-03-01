a6ea19416350653874cbf1158d17c959d926e22e
jyam1 <jasonyam51@gmail.com>
2024-02-29 20:16:02 -0800

    Specify port

--- nerfstudio_flask.py
+++ nerfstudio_flask.py
@@ -32,5 +32,5 @@ def send_video():
     
     return flask.redirect(flask.url_for("home_page"))
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__": 
+    app.run(debug=True, host='0.0.0.0', port=7007)
