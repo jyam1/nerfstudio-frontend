fbf796ec12be92694dc27d0dc51cec8e0ac14a1b
JBicera <joshuaebicera@gmail.com>
2024-03-01 16:25:27 -0800

    Frontend Notification v1

--- nerfstudio_flask.py
+++ nerfstudio_flask.py
@@ -1,4 +1,5 @@
 import flask
+from flask import jsonify
 import subprocess
 import os
 import threading
@@ -8,17 +9,26 @@ import time
 app = flask.Flask(__name__)
 
 output_path = ""
+processing_completed = False
 
 @app.route('/')
 def home_page():
     return flask.render_template("frontend.html")
+
+# Returns response of whether or not video processing is completed
+@app.route('/status')
+def process_status():
+    if processing_completed:
+        return "Video processing complete. Training data in progress..."
+    else:
+        return "Processing video using Colmap..."
 
 @app.route('/', methods=['POST'])
 def send_video():
     uploaded_video = flask.request.files['file']
     
     global output_path
-    
+    global processing_completed
     if uploaded_video.filename != "":
         data_path = uploaded_video.filename + "_data"
         
@@ -33,7 +43,8 @@ def send_video():
     print("Using COLMAP to process video...")
     ns_process_command = ["ns-process-data", "video", "--data", video_path, "--output-dir", output_path]
     subprocess.run(ns_process_command)
-    
+    processing_completed = False
+
     thread = threading.Thread(target=train_model)
     thread.start()
     
@@ -41,8 +52,10 @@ def send_video():
 
 
 def train_model():
+    global processing_completed
+    processing_completed = True
     print("Training...")
     subprocess.run(["ns-train", "splatfacto", "--data", output_path])
     
 if __name__ == "__main__": 
-    app.run(debug=True, host='0.0.0.0', port=7007)
+    app.run(debug=True, host='0.0.0.0', port=8008)
