5d966f8f939da60b62f408b60178e27f839c4df3
jyam1 <jasonyam51@gmail.com>
2024-03-04 09:29:35 -0800

    remove os code

--- nerfstudio_flask.py
+++ nerfstudio_flask.py
@@ -11,13 +11,11 @@ processing_completed = False
 def upload_video(uploaded_video, output_path, video_path):
     if uploaded_video.filename != "":
         data_path = uploaded_video.filename + "_data"
-        
         os.mkdir(data_path)
         
         uploaded_video.save(os.path.join(data_path, uploaded_video.filename))
         
         video_path = os.path.join(data_path, uploaded_video.filename)
-        
         output_path = uploaded_video.filename + "_output"
 
 def process_colmap(video_path, output_path):
@@ -56,7 +54,6 @@ def send_video():
     train_data(output_path)
     
     processing_completed = False
-    os._exit(0)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=7007)
